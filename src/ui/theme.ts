c68169925b38e5294393e36410d07082f6ce2362
Keal Jones <kealjones@gmail.com>
2025-09-23 21:35:41 -0700

    Update theme.ts

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -1,7 +1,7 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  colorSchemes: { light: true, dark: true },
+  colorSchemes: { dark: true },
   cssVariables: {
     colorSchemeSelector: 'media',
   },

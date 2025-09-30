61b4783f94b129de2482057f4eb101462b73a9dc
Keal Jones <kealjones@gmail.com>
2025-09-29 22:15:50 -0700

    zzz

--- src/ui/dialogs/FeedbackDialog.tsx
+++ src/ui/dialogs/FeedbackDialog.tsx
@@ -16,9 +16,9 @@ export function FeedbackDialog({ open, onClose }: DialogProps) {
           src="https://docs.google.com/forms/d/e/1FAIpQLScbtAAitxpL5hDJRzGR-QTDv4ZqlLMDMjI-LZJqhjOGnL4xWg/viewform?embedded=true"
           width="640"
           height="451"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
+          frameBorder={0}
+          marginHeight={0}
+          marginWidth={0}
         >
           Loading…
         </iframe>

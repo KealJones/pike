8306def62bea89a0ee560dd83f12ddeedcf88cf1
Keal Jones <kealjones@gmail.com>
2025-11-17 10:37:50 -0700

    switch to hash based routing to allow deep linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createHashRouter, RouterProvider } from 'react-router';
 import App from './App.tsx';
 import './AppStore.ts';
 import './index.css';
@@ -11,30 +11,27 @@ import PvpTradeCalculatorPage from './pages/PvpTradeCalculator.tsx';
 import { Layout } from './ui/Layout.tsx';
 import theme from './ui/theme.ts';
 
-const router = createBrowserRouter(
-  [
-    {
-      Component: App,
-      children: [
-        {
-          Component: Layout,
-          children: [
-            { path: '/', Component: AnalysisPage },
-            {
-              path: '/pvp-trade-calculator',
-              Component: PvpTradeCalculatorPage,
-            },
-            {
-              path: '/bottle-cap-calculator',
-              Component: BottleCapCalculatorPage,
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  { basename: '/pike/' },
-);
+const router = createHashRouter([
+  {
+    Component: App,
+    children: [
+      {
+        Component: Layout,
+        children: [
+          { path: '/', Component: AnalysisPage },
+          {
+            path: '/pvp-trade-calculator',
+            Component: PvpTradeCalculatorPage,
+          },
+          {
+            path: '/bottle-cap-calculator',
+            Component: BottleCapCalculatorPage,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
